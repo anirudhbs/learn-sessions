927640f5a8e4161ce315a4dde1a42420095cbbd0
anirudhbs <anirudhbs96@gmail.com>
2018-12-27 13:40:07 +0530

    Add page to login

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
+const path = require('path')
+const bodyParser = require('body-parser')
 
 const redisStoreOptions = {
   host: 'localhost',
   port: 6379,
   ttl: 60000
 }
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use(
   session({
@@ -21,15 +26,33 @@ app.use(
   })
 )
 
-app.get('/', (req, res) => {
-  if (req.session.views) {
-    req.session.views += 1
+app.use(express.static('public'))
+
+const checkUser = (req, res, next) => {
+  if (!('email' in req.session)) {
+    res.sendFile(path.join(__dirname, '../public/login.html'))
   } else {
-    req.session.views = 1
+    next()
   }
+}
+
+app.get('/', checkUser, (req, res) => {
+  req.session.views += 1
   res.json({
+    email: req.session.email,
     views: req.session.views
   })
+})
+
+app.post('/login', (req, res) => {
+  const { email } = req.body
+  req.session.email = email
+  req.session.views = 0
+  res.redirect('/')
+})
+
+app.use('*', (req, res) => {
+  res.redirect('/')
 })
 
 const PORT = process.env.PORT || 8080
